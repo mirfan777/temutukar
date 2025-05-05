20f59f798883a6ff6613a474de2363eaeecebcd3
mirfan777 <fcmirfan122@gmail.com>
2025-05-05 21:07:06 +0700

    test

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\TemuTukarController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ChatController;
 
 Route::get('/', function () {
     return view('landing');
@@ -28,11 +27,11 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 
     // chat
-    Route::get('/chat', [ChatController::class, 'index'])->name('chat.index');
-    Route::get('/chat/messages', [ChatController::class, 'fetchMessages'])->name('chat.fetch');
-    Route::post('/chat/send', [ChatController::class, 'sendMessage'])->name('chat.send');
-    Route::delete('/chat/delete/{message}', [ChatController::class, 'deleteMessage'])->name('chat.delete');
-    Route::post('/chat/typing', [ChatController::class, 'typing'])->name('chat.typing');
+    // Route::get('/chat', [ChatController::class, 'index'])->name('chat.index');
+    // Route::get('/chat/messages', [ChatController::class, 'fetchMessages'])->name('chat.fetch');
+    // Route::post('/chat/send', [ChatController::class, 'sendMessage'])->name('chat.send');
+    // Route::delete('/chat/delete/{message}', [ChatController::class, 'deleteMessage'])->name('chat.delete');
+    // Route::post('/chat/typing', [ChatController::class, 'typing'])->name('chat.typing');
 });
 
 require __DIR__.'/auth.php';
